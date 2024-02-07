ea9301ef6c83c403e845f339047738cffff70da7
Rakyan Adhikara <rkyn.aa@gmail.com>
2024-02-07 18:10:00 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'rkynPENA', // Usually your GitHub org/user name.
+  organizationName: 'rkynpena', // Usually your GitHub org/user name.
   projectName: 'jan-docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
